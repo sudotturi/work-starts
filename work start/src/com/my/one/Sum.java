a2353d0db808f953f806e10bd9a30d31c54f58eb
USER <USER@RahulChitturi.lan>
2019-09-21 01:10:10 +0530

    first from eclips

---
+++ work start/src/com/my/one/Sum.java
@@ -0,0 +1,12 @@
+ package com.my.one;
+
+public class Sum {
+ public static void sum(int a, int b) {
+	 System.out.print(a+b);
+ }
+	public static void main(String[] args) {
+	Sum.sum(12,34);
+
+	}
+
+}
